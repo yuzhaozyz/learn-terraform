572fb3f933c6aa867a624d002afd07c74493592e
YuZhao Zhang <yuzhaozyz@gmail.com>
2024-07-30 20:14:35 -0700

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -2,15 +2,15 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "yuzhaozyz"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
